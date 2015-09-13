aea7faa985e990b3b3ccf120a8b8e8c167ddab87
antixaker <proskuraaleksandr93@gmail.com>
2015-09-13 17:04:22 +0300

    Start project

---
+++ ExtentionMethodProject/ExtentionLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionLibrary
+{
+    public static class StringExtentionClass
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ' }).Count();
+        }
+    }
+}
---
+++ ExtentionMethodProject/ExtentionMethodProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtentionLibrary;
+
+
+namespace ExtentionMethodProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your sentence:");
+            int result = Console.ReadLine().WordCount();
+            Console.WriteLine("Sentece has {0} words :)",result);
+            Console.ReadKey();
+        }
+    }
+}
