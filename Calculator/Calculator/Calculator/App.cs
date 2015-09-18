0d62b00ad42003dbbb1f3390e7a4f04925ae0f53
antixaker <proskuraaleksandr93@gmail.com>
2015-09-18 22:53:53 +0300

    Set CalculatorPage as startPage

--- Calculator/Calculator/Calculator/App.cs
+++ Calculator/Calculator/Calculator/App.cs
@@ -11,7 +11,7 @@ namespace Calculator
     {
         public App()
         {
-         
+            MainPage = new NavigationPage(new CalculatorPage());
         }
 
         protected override void OnStart()
